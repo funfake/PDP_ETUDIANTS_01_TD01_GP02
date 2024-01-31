b3812a2040231a48388ea27b12997395bc29b60b
funfake <funfake.electronics@gmail.com>
2024-01-31 09:59:23 +0100

    e3

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 #define SENSOR 33
 #define LED 26
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
 // Définir le type de capteur (DHT22)
 #define DHTTYPE DHT11
 
@@ -13,12 +18,37 @@ DHT_Unified dht(SENSOR, DHTTYPE);
 
 uint32_t delayMS;
 
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+
 void setup() {
   Serial.begin(9600);
 
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  // Activate the timer wakeup
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
   // Initialiser le capteur DHT
   dht.begin();
-
   Serial.println(F("DHTxx Unified Sensor Example"));
 
   // Initialiser la broche LED comme sortie
@@ -28,12 +58,8 @@ void setup() {
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   delayMS = 5000;
-}
 
-void loop() {
-  // Effectuer une mesure toutes les 5s
-  delay(delayMS);
-
+  // From loop
   // Obtenir l'événement de température
   sensors_event_t event;
   dht.temperature().getEvent(&event);
@@ -58,4 +84,12 @@ void loop() {
   } else {
     Serial.println(F("Error reading humidity!"));
   }
+
+  // enter deep sleep
+  Serial.println("Going to sleep now");
+  Serial.flush(); 
+  esp_deep_sleep_start();
+}
+
+void loop() {
 }
